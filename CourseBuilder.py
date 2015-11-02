1d5bcc0424f7a624657d51e9a2dfc7ad4cf2c63a
lidorcg <lidorcg@gmail.com>
2015-11-02 18:41:30 +0200

    - added units html

--- CourseBuilder.py
+++ CourseBuilder.py
@@ -2,33 +2,70 @@ import os
 from CanvasAPy import OAuth
 import CanvasAPy
 
-token = OAuth.get_token_from_file('token')
-api = CanvasAPy.CanvasAPI('0.0.0.0:3000', token)
 
-lid = api.Accounts.get(1)
+def create_course(acc):
+    crs_name = ''
+    for x in os.listdir('.'):
+        if os.path.isdir(x):
+            crs_name = x
+    crs = acc.Courses.new({'name': crs_name})
+    os.chdir(crs_name)
+    create_modules(crs)
 
-crs_name = ""
 
-for x in os.listdir('.'):
-    if os.path.isdir(x) and x != 'CanvasAPy':
-        crs_name = x
-
-crs = lid.Courses.new({'name': crs_name})
-
-os.chdir(crs_name)
-
-mdls = os.listdir('.')
-mdls.sort()
-
-for m in mdls:
-    mdl = crs.Modules.new({'name': m[3:]})
-    os.chdir(m)
-    lsns = os.listdir('.')
-    lsns.sort()
-    for l in lsns:
-        pg = mdl.Pages.new(l[3:])
+def create_modules(crs):
+    modules = os.listdir('.')
+    modules.sort()
+    for m in modules:
+        mdl = crs.Modules.new({'name': m[3:]})
+        os.chdir(m)
+        create_pages(mdl)
     os.chdir('..')
 
 
-# ToDo prettify the code a bit
+def create_pages(mdl):
+    lessons = os.listdir('.')
+    lessons.sort()
+    for l in lessons:
+        pg = mdl.Pages.new({'title': l[3:]})
+        os.chdir(l)
+        create_units(pg)
+        pg.content.update()
+    os.chdir('..')
+
+
+def create_units(pg):
+    units = os.listdir('.')
+    units.sort()
+    page_body = ''
+    for u in units:
+        page_body += new_section(u[3:])
+    pg.content['body'] = page_body
+    os.chdir('..')
+
+
+def new_section(unt):
+    section = """
+    <h1>{}</h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam rutrum tristique tincidunt. Vestibulum ante ipsum
+        primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vestibulum sit amet augue bibendum, ultricies
+        elit vitae, scelerisque ipsum. Vivamus justo dolor, mollis at porttitor in, suscipit quis est. Praesent porta semper
+        massa, non semper nunc dictum quis. Nulla fringilla fringilla hendrerit. Etiam in metus non metus rutrum molestie.
+        Curabitur consectetur finibus molestie. Quisque lorem nunc, blandit a sodales in, mollis at nibh. Maecenas faucibus
+        felis vel mauris semper, in suscipit felis rhoncus. Vestibulum sed leo in augue accumsan pretium. Etiam varius est
+        eget erat ultrices eleifend sed eu mauris. Nulla tincidunt enim arcu, in consectetur orci imperdiet quis. Nullam ac
+        mauris neque. Ut nec hendrerit dolor. In hac habitasse platea dictumst.
+    </p><br>
+    """.format(unt)
+    return section
+
+
+token = OAuth.get_token_from_file('token')
+api = CanvasAPy.CanvasAPI('0.0.0.0:3000', token)
+
+my_account = api.Accounts.get(1)
+
+create_course(my_account)
+
 # ToDo add logging
